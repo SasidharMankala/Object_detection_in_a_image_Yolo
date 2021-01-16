f68974d5443cfea85571f5000ed8ce5d8b6867da
SasidharMankala <60248183+SasidharMankala@users.noreply.github.com>
2021-01-16 21:59:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,86 @@
 import numpy as np
 import argparse
-import time
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO directory")
-ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppression")
-args = vars(ap.parse_args())
 
-# load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image")
+args = vars(ap.parse_args())
+confind = 0.5
+threshold= 0.3
+labelsPath = os.path.sep.join([ "E:\Yoloproject\Yolo_img\yolo-coco\coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
-# initialize a list of colors to represent each possible class label
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
-# derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-configPath = os.path.sep.join([args["yolo"], "yolov3-320.cfg"])
-# load our YOLO object detector trained on COCO dataset (80 classes)
-print("[INFO] loading YOLO from disk...")
+
+
+# np.random.seed(42)
+# COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+weightsPath = os.path.sep.join([ "E:\Yoloproject\Yolo_img\yolo-coco\yolov3.weights"])
+configPath = os.path.sep.join(["E:\Yoloproject\Yolo_img\yolo-coco\yolov3-320.cfg"])
+print("loading YOLO")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-# load our input image and grab its spatial dimensions
 image = cv2.imread(args["image"])
 (H, W) = image.shape[:2]
-# determine only the *output* layer names that we need from YOLO
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-# construct a blob from the input image and then perform a forward
-# pass of the YOLO object detector, giving us our bounding boxes and
-# associated probabilities
 blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-end = time.time()
-# show timing information on YOLO
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-# initialize our lists of detected bounding boxes, confidences, and
-# class IDs, respectively
+layerOutputs = net.forward(ln)
+
+imgGry = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+ret, thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
+contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+
 boxes = []
 confidences = []
 classIDs = []
+count = 0
 
-# loop over each of the layer outputs
+
+
+
 for output in layerOutputs:
-    # loop over each of the detections
     for detection in output:
-        # extract the class ID and confidence (i.e., probability) of
-        # the current object detection
         scores = detection[5:]
         classID = np.argmax(scores)
         confidence = scores[classID]
-        # filter out weak predictions by ensuring the detected
-        # probability is greater than the minimum probability
-        if confidence > args["confidence"]:
-            # scale the bounding box coordinates back relative to the
-            # size of the image, keeping in mind that YOLO actually
-            # returns the center (x, y)-coordinates of the bounding
-            # box followed by the boxes' width and height
+        if confidence > confind:
             box = detection[0:4] * np.array([W, H, W, H])
             (centerX, centerY, width, height) = box.astype("int")
-            # use the center (x, y)-coordinates to derive the top and
-            # and left corner of the bounding box
             x = int(centerX - (width / 2))
             y = int(centerY - (height / 2))
-            # update our list of bounding box coordinates, confidences,
-            # and class IDs
             boxes.append([x, y, int(width), int(height)])
             confidences.append(float(confidence))
             classIDs.append(classID)
 
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
-idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+idxs = cv2.dnn.NMSBoxes(boxes, confidences, confind, threshold)
 
-# ensure at least one detection exists
 if len(idxs) > 0:
-    # loop over the indexes we are keeping
     for i in idxs.flatten():
-        # extract the bounding box coordinates
         (x, y) = (boxes[i][0], boxes[i][1])
         (w, h) = (boxes[i][2], boxes[i][3])
-        # draw a bounding box rectangle and label on the image
-        color = [int(c) for c in COLORS[classIDs[i]]]
-        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-        text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-        cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, color, 2)
-# show the output image
-crop = image[y:y + h, x:x + w]
+        # color = [int(c) for c in COLORS[classIDs[i]]]
+        crop = image[y-5: y+h+5 , x-5: x+w+5]
+        image_name1 = LABELS[classIDs[i]] + '_'+ str(count) + '.png'
+        image_name2 = 'Detected_Objects.png'
+        image_path1 = os.path.join('E:\Yoloproject\Yolo_img\images\Cropped_Images', image_name1 )
+        # image_path2 = os.path.join('E:\Yoloproject\Yolo_img\images\Detected_Image',image_name2)
+        cv2.imwrite(image_path1,crop)
+        
 
-# cv2.imshow("Image", image)
-cv2.imshow("CroppedImage", crop)
 
+        # text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+        # cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv2.imshow(LABELS[classIDs[i]]+ '_'+ str(count), crop)
+        count +=1
+        # rectangle = cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        print(LABELS[classIDs[i]].upper(), 'with a confidence of : ',  round((confidences[i]*100),4),'%')
+
+# cv2.imwrite(image_path2, image)
 cv2.waitKey(0)
 
-# python main.py --image images/toycar.jpg --yolo yolo-coco use this cmd in the folder of main.py
+
